002b7f76aa1c8afbe129c7df1aa595040a6e3375
Michael <michael.r.ness11@gmail.com>
2025-11-08 04:14:38 -0500

    Add express session dependency

--- src/server.js
+++ src/server.js
@@ -1,14 +1,1 @@
-import express from "express";
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello from Wild West Forum!</h1><p>Docker + Node + Nginx working perfectly 🎉</p>");
-});
-
-app.listen(PORT, () => {
-  console.log(`✅ Wild West Forum running on http://localhost:${PORT}`);
-});
+require('../node/src/server');
